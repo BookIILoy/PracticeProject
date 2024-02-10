f5c3607edb5912626978fdaa1c6c943cc26cf9c4
Nikkyjung <nikkyjungalbion@gmail.com>
2024-02-10 20:54:05 +0700

    add demo database

---
+++ practice_db.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 10, 2024 at 02:53 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `practice_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile_img`
+--
+
+CREATE TABLE `profile_img` (
+  `userId` int(11) NOT NULL DEFAULT 0,
+  `userEmail` varchar(64) NOT NULL,
+  `imgId` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `profile_img`
+--
+
+INSERT INTO `profile_img` (`userId`, `userEmail`, `imgId`) VALUES
+(35, 'backendtest@mail.com', 1),
+(28, 'dasds@mail.com', 1),
+(20, 'dds2s@mail.com', 1),
+(19, 'ddss@mail.com', 1),
+(32, 'registertest@mail.com', 1),
+(17, 'tes2t22212@mail.com', 1),
+(16, 'test221@mail.com', 1),
+(18, 'tt2@mail.com', 1),
+(21, 'tteerr@mail.com', 1),
+(27, 'ttttt@mail.com', 1),
+(37, 'ttw2@mail.com', 2),
+(25, 'tukdanai@mail.com', 1),
+(38, 'tytytt@mail.com', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register_users`
+--
+
+CREATE TABLE `register_users` (
+  `userId` int(11) NOT NULL,
+  `firstName` varchar(64) NOT NULL,
+  `lastName` varchar(64) NOT NULL,
+  `userEmail` varchar(64) NOT NULL,
+  `userPhoneNum` varchar(64) DEFAULT NULL,
+  `userPassword` varchar(64) NOT NULL,
+  `userBirth` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `register_users`
+--
+
+INSERT INTO `register_users` (`userId`, `firstName`, `lastName`, `userEmail`, `userPhoneNum`, `userPassword`, `userBirth`) VALUES
+(16, 'testt22', 'Testpuda', 'test221@mail.com', '0221134422', '$2b$10$SCw8CvLPrHXGbPuJRDwQKuxebWcC8phjmJXUWzE6BuAtqL5/80Bfu', '2024-02-02'),
+(17, 'Use2rTest12', 'TestLast', 'tes2t22212@mail.com', '992223321', '$2b$10$./.MNkubRCKravHeRhpIu.jaExqxNltDXO6TWG9u7TWkEuK72WRS6', '0000-00-00'),
+(18, 'testupdate', 'updatelastname', 'tt2@mail.com', '099212341', '$2b$10$S4jkUkvE4RROKvX3nmWOkOuBRdhlQg3ARzpu2.kq3yUaUXSOnyevW', '2022-01-12'),
+(19, 'Testsss', 'GGSSAA', 'ddss@mail.com', '826249966', '$2b$10$JfY.DlygC.NvrYZrwV/6.OOPJFjbRNgaW8xRIz22ZzjH8NBsqPs2C', '2024-01-01'),
+(20, 'Test2', 'Ddsa', 'dds2s@mail.com', '826248855', '$2b$10$uxcGrXyS4Tg63Z7arlpaluKYTUl7212ytF9U4GU6IwSdp3HvFahru', '2024-01-01'),
+(21, 'Test', 'DDs', 'tteerr@mail.com', '812223331', '$2b$10$O/0x4/PdJ2iHUI07vZM0a.FU59LxpD7RSfjUwvds6gS46DfpdmsMe', '2024-01-02'),
+(25, 'ทักษ์ดนัย', 'อุรัมภรณ์', 'tukdanai@mail.com', '826249968', '$2b$10$OD/v04rM./34NcEdagGNyOD7KjKaojYcmQgUbx0eYiHSANA7oV2.O', '2003-06-16'),
+(27, 'ทักษ์ดนัย', 'อุรัมภรณ์', 'ttttt@mail.com', '826249961', '$2b$10$5m3XHmoU3Zu59Kz9Eq6R9O100l6Klv9A2FYhaaOOfSJ9DjdeSbemq', '2002-01-02'),
+(28, 'ทักษ์ดนัย', 'อุรัมภรณ์', 'dasds@mail.com', '826249922', '$2b$10$U3KjxFfalGcLUbR9Ed5Vo.6Mt65yKN1ogZilww9K1Po9lNOO.1bf.', '2022-12-01'),
+(32, 'Tester', 'Register ', 'registertest@mail.com', '826249900', '$2b$10$rNWM7iB5s9nBw830cGMNKOV8D/CxzaZPA1ce8UO0fuXOnekts983i', '2024-02-01'),
+(35, 'Gaypoon', 'TestLast1', 'backendtest@mail.com', '0818667784', '$2b$10$qiDoxysTFN4tG1iezWI/VOBI0KeESRUiA33GnLSAoCC79jHE93mHe', '2003-02-02'),
+(37, 'jjjhhhfff', 'subimg', 'ttw2@mail.com', '0818667755', '$2b$10$TSC8ee/wpZChQEgo2rL5LuKTsbQWa0ZeEr2bNBVkuG6qiiNBSMTXe', '2024-01-02'),
+(38, 'ttewt', 'trrtrr', 'tytytt@mail.com', '0826248868', '$2b$10$c.73YsXlBcVvxZgDvxn7Yey444kAAM4anO4FajXTLEauxbVmU7G52', '2002-02-01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `profile_img`
+--
+ALTER TABLE `profile_img`
+  ADD KEY `fk_userId` (`userId`);
+
+--
+-- Indexes for table `register_users`
+--
+ALTER TABLE `register_users`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `userEmail` (`userEmail`),
+  ADD UNIQUE KEY `userPhoneNum` (`userPhoneNum`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `register_users`
+--
+ALTER TABLE `register_users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `profile_img`
+--
+ALTER TABLE `profile_img`
+  ADD CONSTRAINT `fk_userId` FOREIGN KEY (`userId`) REFERENCES `register_users` (`userId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
